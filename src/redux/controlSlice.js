717ca03464a4fa404312a96f30b45314ff597dbd
Amathia <igor.grosul@outlook.com>
2023-06-26 14:45:45 +0300

    1

---
+++ src/redux/controlSlice.js
@@ -0,0 +1,12 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {};
+
+const controlSlice = createSlice({
+  name: "control",
+  initialState,
+  reducers: {},
+});
+
+export const {} = controlSlice.actions;
+export default controlSlice.reducer;
